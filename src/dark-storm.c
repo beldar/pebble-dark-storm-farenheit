f61e7332aed68b7037c529a924cb7fff0dfa940d
Martí Planellas <beldar.cat@gmail.com>
2015-03-19 16:45:35 +0000

    Making it compile

--- src/dark-storm.c
+++ src/dark-storm.c
@@ -12,6 +12,10 @@ GFont *font16;
 static GBitmap *image = NULL;
 static BitmapLayer *image_layer;
 
+#define TOTAL_TIME_DIGITS 4
+static GBitmap *s_time_digits[TOTAL_TIME_DIGITS];
+static BitmapLayer *s_time_digits_layers[TOTAL_TIME_DIGITS];
+
 const int IMAGE_RESOURCE_IDS[NUMBER_OF_IMAGES] = {
   RESOURCE_ID_CLEAR_DAY,
   RESOURCE_ID_CLEAR_NIGHT,
@@ -27,16 +31,16 @@ const int IMAGE_RESOURCE_IDS[NUMBER_OF_IMAGES] = {
 };
 
 static const int BIG_DIGIT_IMAGE_RESOURCE_IDS[] = {
-    RESOURCE_ID_IMAGE_NUM_0,
-    RESOURCE_ID_IMAGE_NUM_1,
-    RESOURCE_ID_IMAGE_NUM_2,
-    RESOURCE_ID_IMAGE_NUM_3,
-    RESOURCE_ID_IMAGE_NUM_4,
-    RESOURCE_ID_IMAGE_NUM_5,
-    RESOURCE_ID_IMAGE_NUM_6,
-    RESOURCE_ID_IMAGE_NUM_7,
-    RESOURCE_ID_IMAGE_NUM_8,
-    RESOURCE_ID_IMAGE_NUM_9
+    RESOURCE_ID_NUM_0,
+    RESOURCE_ID_NUM_1,
+    RESOURCE_ID_NUM_2,
+    RESOURCE_ID_NUM_3,
+    RESOURCE_ID_NUM_4,
+    RESOURCE_ID_NUM_5,
+    RESOURCE_ID_NUM_6,
+    RESOURCE_ID_NUM_7,
+    RESOURCE_ID_NUM_8,
+    RESOURCE_ID_NUM_9
 };
 
 enum {
@@ -90,35 +94,36 @@ static void window_load(Window *window) {
   font12 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MYRIAD_PRO_REGULAR_12));
   font16 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MYRIAD_PRO_REGULAR_16));
 
-  // create time layer - this is where time goes
-  text_time_layer = text_layer_create(GRect(8, 40, 144-7, 168-92));
-  text_layer_set_text_color(text_time_layer, GColorWhite);
-  text_layer_set_background_color(text_time_layer, GColorClear);
-  text_layer_set_font(text_time_layer, font49);
-  layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
 
   // create date layer - this is where the date goes
-  text_date_layer = text_layer_create(GRect(8, 10, 144-10, 24));
+  text_date_layer = text_layer_create(GRect(144-32, 6, 32, 10));
   text_layer_set_text_alignment(text_date_layer, GTextAlignmentRight);
   text_layer_set_text_color(text_date_layer, GColorWhite);
   text_layer_set_background_color(text_date_layer, GColorClear);
-  text_layer_set_font(text_date_layer, font21);
+  text_layer_set_font(text_date_layer, font16);
   layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
     
   // create batery layer
-  text_batt_layer = text_layer_create(GRect(10, 10, 144-10, 24));
+  text_batt_layer = text_layer_create(GRect(168-6, 120, 20, 10));
   text_layer_set_text_alignment(text_batt_layer, GTextAlignmentLeft);
   text_layer_set_text_color(text_batt_layer, GColorWhite);
   text_layer_set_background_color(text_batt_layer, GColorClear);
-  text_layer_set_font(text_batt_layer, font21);
+  text_layer_set_font(text_batt_layer, font12);
   layer_add_child(window_layer, text_layer_get_layer(text_batt_layer));
 
   // create temperature layer - this is where the temperature goes
   text_temp_layer = text_layer_create(GRect(80, 108, 144-80, 168-108));
   text_layer_set_text_color(text_temp_layer, GColorWhite);
   text_layer_set_background_color(text_temp_layer, GColorClear);
-  text_layer_set_font(text_temp_layer, font39);
+  text_layer_set_font(text_temp_layer, font16);
   layer_add_child(window_layer, text_layer_get_layer(text_temp_layer));
+    
+  // Create time and date layers
+  GRect dummy_frame = GRect(0, 0, 0, 0);
+  for (int i = 0; i < 4; ++i) {
+    s_time_digits_layers[i] = bitmap_layer_create(dummy_frame);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_time_digits_layers[i]));
+  }
     
   handle_battery(battery_state_service_peek());
   battery_state_service_subscribe(&handle_battery);
@@ -148,31 +153,53 @@ static void app_message_init(void) {
   app_message_register_inbox_received(in_received_handler);
 }
 
+static void set_container_image(GBitmap **bmp_image, BitmapLayer *bmp_layer, const int resource_id, GPoint origin) {
+  GBitmap *old_image = *bmp_image;
+
+  *bmp_image = gbitmap_create_with_resource(resource_id);
+#ifdef PBL_PLATFORM_BASALT
+  GRect bitmap_bounds = gbitmap_get_bounds((*bmp_image));
+#else
+  GRect bitmap_bounds = (*bmp_image)->bounds;
+#endif
+  GRect frame = GRect(origin.x, origin.y, bitmap_bounds.size.w, bitmap_bounds.size.h);
+  bitmap_layer_set_bitmap(bmp_layer, *bmp_image);
+  layer_set_frame(bitmap_layer_get_layer(bmp_layer), frame);
+
+  if (old_image != NULL) {
+  	gbitmap_destroy(old_image);
+  }
+}
+
+static unsigned short get_display_hour(unsigned short hour) {
+  if (clock_is_24h_style()) {
+    return hour;
+  }
+
+  unsigned short display_hour = hour % 12;
+  return display_hour ? display_hour : 12;
+}
 // show the date and time every minute
 void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   // Need to be static because they're used by the system later.
-  static char time_text[] = "00:00";
   static char date_text[] = "Xxxxxxxxx 00";
-
-  char *time_format;
-
   strftime(date_text, sizeof(date_text), "%B %e", tick_time);
   text_layer_set_text(text_date_layer, date_text);
 
+  unsigned short display_hour = get_display_hour(tick_time->tm_hour);
+  set_container_image(&s_time_digits[0], s_time_digits_layers[0], BIG_DIGIT_IMAGE_RESOURCE_IDS[display_hour / 10], GPoint(2, 2));
+  set_container_image(&s_time_digits[1], s_time_digits_layers[1], BIG_DIGIT_IMAGE_RESOURCE_IDS[display_hour % 10], GPoint(54, 2));
 
-  if (clock_is_24h_style()) {
-    time_format = "%R";
-  } else {
-    time_format = "%I:%M";
+  set_container_image(&s_time_digits[2], s_time_digits_layers[2], BIG_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_min / 10], GPoint(2, 74));
+  set_container_image(&s_time_digits[3], s_time_digits_layers[3], BIG_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_min % 10], GPoint(54, 74));
+
+  if (!clock_is_24h_style()) {
+    if (display_hour / 10 == 0) {
+    	layer_set_hidden(bitmap_layer_get_layer(s_time_digits_layers[0]), true);
+    } else {
+    	layer_set_hidden(bitmap_layer_get_layer(s_time_digits_layers[0]), false);
+    }
   }
-
-  strftime(time_text, sizeof(time_text), time_format, tick_time);
-
-  if (!clock_is_24h_style() && (time_text[0] == '0')) {
-    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
-  }
-
-  text_layer_set_text(text_time_layer, time_text);
 }
 
 static void init(void) {
